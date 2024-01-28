8cdea65df95cf8316a23bed586ef31b990bcc0c4
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-01-28 16:25:57 +0200

    Added files

--- GREAT_KART/carts/models.py
+++ GREAT_KART/carts/models.py
@@ -30,5 +30,8 @@ class CartItem(models.Model):
         default=True,
     )
 
+    def sub_total(self):
+        return self.product.price * self.quantity
+
     def __str__(self):
         return self.product
