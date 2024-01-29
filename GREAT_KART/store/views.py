442feaeb0b2ec75c5b17e5b0a9548ac9803c630f
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-01-29 13:07:40 +0200

    Added files

--- GREAT_KART/store/views.py
+++ GREAT_KART/store/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from GREAT_KART.carts.models import CartItem
@@ -13,14 +14,21 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
+
     }
 
     return render(request, 'store/store.html', context)
