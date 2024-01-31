8f0f04e93dfad3a5388487c6d3be38d65f5c759d
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-01-31 17:48:42 +0200

    Added files

--- GREAT_KART/accounts/forms.py
+++ GREAT_KART/accounts/forms.py
@@ -17,6 +17,15 @@ class RegistrationForm(forms.ModelForm):
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
 
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                'Password does not match!'
+            )
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
@@ -26,3 +35,5 @@ class RegistrationForm(forms.ModelForm):
         self.fields['email'].widget.attrs['placeholder'] = 'Enter Email Address'
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+
+
--- GREAT_KART/accounts/views.py
+++ GREAT_KART/accounts/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 
 from GREAT_KART.accounts.forms import RegistrationForm
+from GREAT_KART.accounts.models import Account
 
 
 def register(request):
-    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            username = email.split('@')[0]
+            user = Account.object.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+    else:
+        form = RegistrationForm()
     context = {
         'form': form,
     }
