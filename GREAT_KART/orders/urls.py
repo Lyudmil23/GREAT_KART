59416790016c360f837a756085afd48a38bfce81
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-02-03 21:16:41 +0200

    Added files

--- GREAT_KART/orders/urls.py
+++ GREAT_KART/orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from GREAT_KART.orders.views import place_order
+from GREAT_KART.orders.views import place_order, payments
 
 urlpatterns = [
-    path('place_order/', place_order, name='place_order')
+    path('place_order/', place_order, name='place_order'),
+    path('payments/', payments, name='payments'),
 
 ]
