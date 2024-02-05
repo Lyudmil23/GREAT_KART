55b199b87f7aff5304ad484f149a84e37cf456f0
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-02-05 11:20:06 +0200

    Added files

--- GREAT_KART/store/views.py
+++ GREAT_KART/store/views.py
@@ -1,12 +1,14 @@
+from django.contrib import messages
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from GREAT_KART.carts.models import CartItem
 from GREAT_KART.carts.views import _cart_id
 from GREAT_KART.category.models import Category
-from GREAT_KART.store.models import Product
+from GREAT_KART.store.forms import ReviewForm
+from GREAT_KART.store.models import Product, ReviewRating
 
 
 def store(request, category_slug=None):
@@ -63,3 +65,29 @@ def search(request):
             'product_count': product_count,
         }
     return render(request, 'store/store.html', context)
+
+
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == 'POST':
+        try:
+            reviews = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            form.save()
+            messages.success(request, 'Thank you! Your review has been updated.')
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(request, 'Thank you! Your review has been submitted.')
+                return redirect(url)
+
+    return render(request, 'store/submit_review.html')
