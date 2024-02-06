0fbae01d6397128cd18ad23315c973c391effd63
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-02-06 11:24:31 +0200

    Added files

--- GREAT_KART/accounts/views.py
+++ GREAT_KART/accounts/views.py
@@ -18,6 +18,8 @@ from GREAT_KART.carts.models import Cart, CartItem
 from GREAT_KART.carts.views import _cart_id
 import requests
 
+from GREAT_KART.orders.models import Order
+
 
 def register(request):
     if request.method == 'POST':
@@ -148,7 +150,12 @@ def activate(request, uidb64, token):
 
 @login_required(login_url='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
+    orders_count = orders.count()
+    context = {
+        'orders_count': orders_count,
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def forgotPassword(request):
